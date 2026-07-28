5807772c7c959621349ab486dabf8006b951bb77
KUMAR, Aarav <akumarai@connect.ust.hk>
2026-07-28 10:56:05 +0800

    Add: frequency-of-the-most-frequent-element - submission-7

---
+++ Data Structures & Algorithms/frequency-of-the-most-frequent-element/submission-7.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int l = 0; long long window = 0; int n = nums.size(); int freq = 1;
+
+        for (int r = 1; r < n; r++) {
+
+            window += (long long)((nums[r] - nums[r-1]) * (r-l));
+
+            while (window > k) {
+                window -= (nums[r] - nums[l]);
+                l++;
+            }
+
+            freq = max(freq, r-l+1);
+        }
+        return freq;
+    }
+};
