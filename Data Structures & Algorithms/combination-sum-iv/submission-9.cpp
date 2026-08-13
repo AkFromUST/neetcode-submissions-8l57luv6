4123cfd4fcd4d741f1bdf795706532600d6c9824
KUMAR, Aarav <akumarai@connect.ust.hk>
2026-08-13 11:36:52 +0800

    Add: combination-sum-iv - submission-9

---
+++ Data Structures & Algorithms/combination-sum-iv/submission-9.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    
+    int _dfs(vector<int>& nums, int i, int target, unordered_map<int, int>& cache) {
+        if (cache.count(target)) { return cache[target]; }
+    
+        int res = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (target < 0) {break;}
+            res += _dfs(nums, i, target - nums[i], cache);
+        }
+
+        return cache[target] = res;
+    }
+    
+    int combinationSum4(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end()); unordered_map<int, int> cache = {};
+        cache[0] = 1;
+        
+        return _dfs(nums, 0, target, cache);
+    }
+
+// NO DP
+
+    // int _dfs(vector<int>& nums, int i, int target) {
+    //     if (i == nums.size() || target < 0) {
+    //         return 0;
+    //     }
+        
+    //     if (target == 0) { return 1; }
+
+    //     int res = 0;
+
+    //     for (int i = 0; i < nums.size(); ++i) {
+    //         if (nums[i] > target) { return res; }
+    //         res += _dfs(nums, i, target - nums[i]);
+    //     }
+
+    //     return res;
+    // }
+    
+    // int combinationSum4(vector<int>& nums, int target) {
+    //     sort(nums.begin(), nums.end()); vector<vector<int>> cache(nums.size(), vector<int>(target+1, -1));
+        
+    //     return _dfs(nums, 0, target);
+    // }
+};
